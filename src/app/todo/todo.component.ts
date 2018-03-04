302d1882bd8f3801d460744ec9d6e24c61473b21
aniket umbc <aniket.umbc@gmail.com>
2018-03-04 17:20:00 +0530

    Added component HTML for List

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import  { TodoService } from '../todo/shared/todo.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers:[TodoService]
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+toDoListArray :any[];
+  constructor(private toDoService: TodoService ) { }
 
   ngOnInit() {
-  }
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item=>{
+    this.toDoListArray=[];
+    item.forEach(element=>{
+      var x = element.payload.toJSON();  //return JSON Object 
+      x["$key"]=element.key;
+      this.toDoListArray.push(x);
+    })    
 
+    //sort array isChecked false -> true
+    this.toDoListArray.sort((a,b)=>{
+      return a.isChecked - b.isChecked;
+    })    
+  });
+  }
+  onAdd(itemTitle){
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value=null; //reset textbox by null
+  }
 }
